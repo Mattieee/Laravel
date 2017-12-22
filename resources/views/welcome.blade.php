96f066ccfd69f222f2ee0c7dc02f51edaf89b6ec
Unknown <Matthieu.royer78@gmail.com>
2017-12-22 17:47:08 +0100

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Salut petit Ali
+                    Salut 
                 </div>
 
                 <div class="links">
