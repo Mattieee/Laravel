647b1ea433b8b233f447d418aa4265c2f895a933
Unknown <bouaidaali@hotmail.fr>
2017-12-22 18:00:48 +0100

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Salut 
+                    Salut fqfqfqefhyhyhyhyh
                 </div>
 
                 <div class="links">
